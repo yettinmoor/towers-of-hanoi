1508c693a37e9269d91d94de0bf164a5c9f92187
Nicolas Lindström <nicolas.lindstroem@protonmail.com>
2020-11-18 23:09:42 +0100

    formatting

--- hanoi.zig
+++ hanoi.zig
@@ -13,10 +13,9 @@ pub fn main() !void {
     for (towers) |*t|
         t.* = Tower.init(allocator);
 
-    {
-        var i: u8 = discs;
-        while (i > 0) : (i -= 1) try towers[0].append(i);
-    }
+    var i: u8 = discs;
+    while (i > 0) : (i -= 1)
+        try towers[0].append(i);
 
     try printTowers();
     try move(discs, &towers[0], &towers[2], &towers[1]);
@@ -27,10 +26,11 @@ var moves: std.meta.Int(.unsigned, discs) = 0;
 fn move(n: u8, from: *Tower, to: *Tower, aux: *Tower) anyerror!void {
     if (n == 1) {
         try to.append(from.pop());
+        moves += 1;
+        // Print
         const stdout = std.io.getStdOut().writer();
         try stdout.print("\x1b[{}F", .{discs});
         std.time.sleep(@as(u64, 3e9) / (1 << discs));
-        moves += 1;
         try printTowers();
     } else {
         try move(n - 1, from, aux, to);
@@ -41,15 +41,15 @@ fn move(n: u8, from: *Tower, to: *Tower, aux: *Tower) anyerror!void {
 
 fn printTowers() !void {
     const stdout = std.io.getStdOut().writer();
-    const msg = "#" ** discs;
-    const pad = " " ** discs;
+    const disc = "#" ** discs;
+    const padding = " " ** discs;
 
     var i: u8 = discs - 1;
     while (true) : (i -= 1) {
         for (towers) |t| {
-            const msg_slice = if (t.items.len <= i) "" else msg[0..t.items[i]];
-            const pad_slice = if (t.items.len <= i) pad else pad[0..(discs - t.items[i])];
-            try stdout.print("{0}{1}|{1}{0}", .{ pad_slice, msg_slice });
+            const disc_slice = if (t.items.len <= i) "" else disc[0..t.items[i]];
+            const padding_slice = if (t.items.len <= i) padding else padding[0..(discs - t.items[i])];
+            try stdout.print("{0}{1}|{1}{0}", .{ padding_slice, disc_slice });
         }
         if (i == discs - 1) {
             try stdout.print("  Moves: {}", .{moves});
